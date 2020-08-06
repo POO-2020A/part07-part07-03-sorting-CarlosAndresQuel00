1c7007ac014a0cdc9dccb186a96e79b69e4422d1
CarlosAndresQuel00 <66259796+CarlosAndresQuel00@users.noreply.github.com>
2020-08-05 21:41:08 -0500

    Add files via upload

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,9 +1,60 @@
 
+import java.util.Arrays;
+
 public class MainProgram {
 
     public static void main(String[] args) {
-        // write your test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[] array = {8, 3, 7, 9, 1, 2, 4};
+        
+        MainProgram.sort(array);
     }
 
+    public static int smallest(int[] array) {
+        int min = 9999;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int min = 9999;
+        int pos = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                pos = i;
+            }
+        }
+        return pos;
+    }
+
+    public static int indexOfSmallestFrom(int[] array, int startIndex) {
+        int min = 9999;
+        int pos = 0;
+        for (int i = startIndex; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                pos = i;
+            }
+        }
+        return pos;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    public static void sort(int[] array) {
+        int pos;
+        for (int i = 0; i < array.length; i++) {
+            pos = indexOfSmallestFrom(array, i);
+            swap(array, i, pos);
+            System.out.println(Arrays.toString(array));
+        }
+    }
 }
